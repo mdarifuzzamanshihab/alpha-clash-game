a4157b4d4151ba29b010dcb0265c006d49d1dcc3
mdarifuzzamanshihab <riyad.md5445@gmail.com>
2024-02-13 22:20:57 +0600

    added pageSwitch section

---
+++ scripts/alpha-clash.js
@@ -0,0 +1,6 @@
+function play() {
+  const homeSection = document.getElementById("home-screen");
+  homeSection.classList.add("hidden");
+  const playgroundSection = document.getElementById("play-ground");
+  playgroundSection.classList.remove("hidden");
+}
